4fbaa8a5dac8c717c8955f4174c5b9971c139786
MewKay <125735249+MewKay@users.noreply.github.com>
2023-05-26 22:03:00 +0300

    Put event on digit buttons to display value

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let numberA;
 let numberB;
 let operator;
+let numberToDisplay = "";
+let displaySection = document.querySelector(".display");
+let digits = document.querySelectorAll(".digits");
+
+digits.forEach(digit => digit.addEventListener("click", function (e) {
+  display(e.target);
+}));
+
+function display(digitButton) {
+  numberToDisplay += digitButton.textContent;
+  displaySection.textContent = numberToDisplay;
+}
 
 function add(numberA, numberB){
   return numberA+numberB;
